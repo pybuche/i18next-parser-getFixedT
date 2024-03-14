a2b7f1b23db9dc6f16c699986acdeaa653d8ba13
pybuche <pierrick.boutruche@gmail.com>
2024-03-14 19:25:16 +0100

    fixes

--- app/t.js
+++ app/t.js
@@ -1,4 +1,4 @@
-import { i18n } from "./i18n"
+import { i18n } from "../i18n"
 
 const text = i18n.t("hello", { ns: "a" })
 const text2 = i18n.t("hello2", { ns: "a" })
--- i18n/i18next-parser-config.js
+++ i18n/i18next-parser-config.js
@@ -1,7 +1,7 @@
 export default {
   defaultNamespace: "common",
-  input: ["../*.js"],
-  output: "../locales/$LOCALE/$NAMESPACE.json",
+  input: ["../app/*.js"],
+  output: "locales/$LOCALE/$NAMESPACE.json",
   defaultValue: (_lng, ns, key) => `${ns}_${key}`,
   lineEnding: "after",
   sort: true,
