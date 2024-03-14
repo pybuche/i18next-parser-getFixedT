c94f09cd0a64f5239dfac741ee21192cbe5549cc
pybuche <pierrick.boutruche@gmail.com>
2024-03-14 18:44:38 +0100

    example: can't extract relative parent translations

--- config/i18next-parser-config.js
+++ config/i18next-parser-config.js
@@ -1,7 +1,7 @@
 export default {
   defaultNamespace: "common",
-  input: ["./t.js", "./getFixedT.js"],
-  output: "./locales/$LOCALE/$NAMESPACE.json",
+  input: ["../*.js"],
+  output: "../locales/$LOCALE/$NAMESPACE.json",
   defaultValue: (_lng, ns, key) => `${ns}_${key}`,
   lineEnding: "after",
   sort: true,
